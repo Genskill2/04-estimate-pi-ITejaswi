829924d526c478eacd587d74ae9cd8cb73db0840
ITejaswi <84660145+ITejaswi@users.noreply.github.com>
2021-06-02 20:37:42 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -27,7 +27,7 @@ int main(void) {
 float wallis_pi(int count){
     float pi = 1.0;
     for (int i = 1; i < count; i++){
-      float num = 4.0*i*I;
+      float num = 4.0*i*i;
       pi* = num/(num-1);
     }
      return pi*2;
