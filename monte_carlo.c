b5d5beef18c1d359983a74c5db9dd4125646308b
ITejaswi <84660145+ITejaswi@users.noreply.github.com>
2021-06-02 21:05:58 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -43,11 +43,11 @@ float mc_pi(int n)
 	int c=0,s=n;
 	
 	for(int i=0; i<n; i++){
-	float x,y,l;
+	float x,y,dis;
 	x=frandom();
 	y=frandom();
-	l=x*x+y*y;
-	if(l<=1){
+	dis=x*x+y*y;
+	if(dis<=1){
 	c++;
 	}
 	}
